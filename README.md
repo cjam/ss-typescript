6cb644206b7acad09382ce9c258a1ed8458b3efd
cjam <colter.mcquay@gmail.com>
2013-12-25 21:59:52 -0600

    Committing the typescript formatter implementation. Everything seems to be working.  Might not want to rethrow the compilation exception because it will kill the server, but thats an easy change.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-ss-typescript
-=============
+# Typescript (JS) wrapper for SocketStream 0.3
+
+Allows you to use [Typescript](http://www.typescriptlang.org/) files (.ts) in your SocketStream project.
+
+
+### Instructions
+
+Add `ss-typescript` to your application's `package.json` file and then add this line to app.js:
+
+```javascript
+ss.client.formatters.add(require('ss-typescript'));
+```
---
+++ wrapper.js
@@ -0,0 +1,59 @@
+// Typescript (JS) wrapper for SocketStream 0.3
+
+var fs = require('fs'),
+tsapi = require('typescript.api');
+
+// Build a javascript error from the TS Diagnostics object returned by the TS compiler
+function makeTSError (units) {
+	var errors = "";
+    for(var n in units) {
+        for(var m in units[n].diagnostics) {
+			// just combine all of the errors and throw them at the end.
+            errors += "\n"+ units[n].diagnostics[m].toString();
+        }
+    }
+	return new Error(errors);	
+}
+
+function handleError(message, error, callback){
+	console.log(String.prototype.hasOwnProperty('red') && message.red || message);
+	callback("Error compiling to JavaScript: " + error.stack);
+	throw new Error(error);
+}
+
+exports.init = function(root, config) {
+
+  return {
+
+    name: 'Typescript',
+
+    extensions: ['ts'],
+
+    assetType: 'js',
+
+    contentType: 'text/javascript; charset=utf-8',
+
+    compile: function(path, options, cb) {
+		// resolve the current typescript file
+		tsapi.resolve([path],function(resolved){
+			if(!tsapi.check(resolved)){
+				handleError("Typescript Resolution Error in: "+path, makeTSError(resolved), cb);
+			}else{
+				// compile the current typescript file
+				tsapi.compile(resolved, function(compiled){
+					if(!tsapi.check(compiled)){
+						console.log("compile error");
+						handleError("Typescript Compilation Error in: "+path, makeTSError(compiled), cb);
+					}else{
+						if(compiled[0] && compiled[0].content){
+							cb(compiled[0].content);
+						}else{
+							handleError("Unknown Error.", new Error("Compiled content is missing from the compiled typescript object"), cb);
+						}
+					}				
+				});			
+			}		
+		});     		
+    }
+  };
+};
